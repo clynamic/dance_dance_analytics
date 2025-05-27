8b9f580b5503d350bdf552301ab056848aa68b38
clragon <clragon@clynamic.net>
2025-05-27 13:18:20 +0200

    updated entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,18 @@ POSTGRES_USER=${POSTGRES_USER%%:*}
 POSTGRES_PASSWORD=${DATABASE_URL#*://${POSTGRES_USER}:}
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD%%@*}
 
-POSTGRES_HOST=${DATABASE_URL#*@}
+POSTGRES_HOSTPORT=${DATABASE_URL#*@}
+POSTGRES_HOST=${POSTGRES_HOSTPORT%%/*}
+POSTGRES_PORT=${POSTGRES_HOST#*:}
 POSTGRES_HOST=${POSTGRES_HOST%%:*}
 
 MAX_WAIT=60
 WAIT_INTERVAL=2
 elapsed=0
 
-echo "Waiting for Postgres to be available at $POSTGRES_USER@$POSTGRES_HOST:$POSTGRES_PORT"
+echo "Waiting for Postgres to be available at $POSTGRES_USER:*****@$POSTGRES_HOST:$POSTGRES_PORT"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\q' >/dev/null 2>&1; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "${POSTGRES_PORT:-5432}" -c '\q' >/dev/null 2>&1; do
   [ $elapsed -ge $MAX_WAIT ] && echo "Postgres is unavailable. Exiting." && exit 1
   sleep $WAIT_INTERVAL
   elapsed=$((elapsed + WAIT_INTERVAL))
